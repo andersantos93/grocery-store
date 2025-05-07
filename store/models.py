a54d0233cf9268959fa91eef763c2f900868ee0f
Anderson Santos <anderson.chenko1993@gmail.com>
2025-05-06 22:53:20 -0600

    add model product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+  name = models.CharField(max_length=200, unique=True, null=False, help_text="Enter a product")
+  price = models.FloatField(null=False, blank=False, default=0.0, validators=[MinValueValidator(0.0)], help_text="Price of the product")
